d73fea2839ab75e54b6ba099f01d1af9f6cdc65d
JohnVictorReis <johnvictor.trabalho@gmail.com>
2022-07-20 23:31:41 -0300

    Adicionado formulario padrão para a aba usuario

--- biblioteca/src/components/MenuUsuario.vue
+++ biblioteca/src/components/MenuUsuario.vue
@@ -21,12 +21,27 @@ export default {
 
 <template>
   <nav id="menu-top">
-    <router-link to="/screen_user"><span>Home</span></router-link>
-    <router-link to="/carrinho"><span>Carrinho {{ qtdCarrinho }}</span>
+    <v-img src="../assets/biblioteca.png" ></v-img>
+    <router-link to="/screen_user"><span>HOME</span></router-link>
+    <router-link to="/carrinho"><span class="carrinhoMenu">CARRINHO ( {{ qtdCarrinho }} )</span>
     </router-link>
 
   </nav>
 </template>
 
 <style>
+
+#menu-top {
+  background-color: #171a21;
+  width: 96%;
+  height: 10%;
+  padding: 2.5%;
+  color: #c4c2c0;
+  text-align: center;
+}
+
+.carrinhoMenu {
+ margin-left: 1100px;
+
+}
 </style>
