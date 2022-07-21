ab470ae3b00043d8de5804d95c93e6b4b768d346
JohnVictorReis <johnvictor.trabalho@gmail.com>
2022-07-20 23:32:29 -0300

    Nav reformulado e finalizado

--- biblioteca/src/components/NavBar.vue
+++ biblioteca/src/components/NavBar.vue
@@ -6,9 +6,16 @@ import style from '@/assets/style.css';
 
 <template>
   <nav id="menu-top">
-    <router-link to="/"><span>Homew</span></router-link>
-    <router-link to="/admin"><span>Administração</span></router-link>
-    <router-link to="/cad_user" class="cad_user"><span>Adicionar Usuário</span></router-link>
-    <input type="text" v-model="input" placeholder="Procurar livros..." />
+    <router-link to="/"><span>HOME</span></router-link>
+    <router-link to="/admin"><span>ADMINISTRAÇÃO</span></router-link>
+    <router-link to="/cad_user" class="cad_user"><span>CADASTRO DE USUÁRIO</span></router-link>
+    <input type="text" class="pesquisa" v-model="input" placeholder="Procurar livros..." />
   </nav>
 </template>
+
+
+<style>
+.pesquisa{
+  margin-left: 650px;
+}
+</style>
